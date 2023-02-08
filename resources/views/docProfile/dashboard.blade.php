16f9118530dd3a2c28e30d29e16efce833888db4
T30G10v4 <matteo.giovagnotti@gmail.com>
2023-02-08 17:15:00 +0100

    ADD: docprofile area

---
+++ resources/views/docProfile/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.doc_admin')
+
+@section('content')
+    <h2>PAGINA DOTTORE</h2>
+@endsection
---
+++ resources/views/layouts/doc_admin.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Profile</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Usando Vite -->
+    @vite(['resources/js/app.js'])
+</head>
+
+<body>
+    <div style="height: 100vh" class="bg-dark">
+        {{-- Header --}}
+        <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-2 shadow"
+            style="border-bottom: 1px solid white; height: 50px">
+            <div class="row justify-content-between">
+                <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">
+                    <em>My Profile</em> </a>
+                <button class="navbar-toggler position-absolute d-md-none collapsed" type="button"
+                    data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+
+            {{-- logout --}}
+            <div class="navbar-nav">
+                <div class="nav-item text-nowrap ms-2">
+                    <a class="nav-link text-danger" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </div>
+            {{-- /logout --}}
+
+        </header>
+        {{-- /Header --}}
+
+        <div class="container-fluid bg-dark">
+
+            <div class="row " style="height: calc(100% - 50px)">
+                {{-- Sidebar --}}
+                <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark navbar-dark sidebar collapse"
+                    style="border-right: 1px solid white; scroll:auto;">
+                    <div class="position-sticky pt-3">
+                        {{-- <ul class="nav flex-column">
+                            <li class="nav-item">
+                                
+                                <a class="nav-link text-white {{ Route::currentRouteName() === 'admin.dashboard' ? 'bg-secondary' : '' }}"
+                                    href="{{ route('admin.dashboard') }}">
+                                    <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i>
+                                    Dashboard
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link text-white {{ Route::currentRouteName() === 'admin.projects.index' ? 'bg-secondary' : '' }}"
+                                    href="{{ route('admin.projects.index') }}">
+                                    <i class="fa-solid fa-list"></i>
+                                    My Messages
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link text-white {{ Route::currentRouteName() === 'admin.types.index' ? 'bg-secondary' : '' }}"
+                                    href="{{ route('admin.types.index') }}">
+                                    <i class="fa-solid fa-folder"></i>
+                                    My Reviews
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link text-white {{ Route::currentRouteName() === 'admin.technologies.index' ? 'bg-secondary' : '' }}"
+                                    href="{{ route('admin.technologies.index') }}">
+                                    <i class="fa-solid fa-microchip"></i>
+                                    My Statistics
+                                </a>
+                            </li>
+
+
+                        </ul> --}}
+                    </div>
+                </nav>
+                {{-- /Sidebar --}}
+
+                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4" style="scroll:auto">
+                    @yield('content')
+                </main>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
