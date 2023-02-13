1ae62485f6d02f4130dbfde4031b113c4aef92de
T30G10v4 <matteo.giovagnotti@gmail.com>
2023-02-13 18:04:28 +0100

    ADD: spec list api

---
+++ app/Http/Controllers/Api/SpecsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Specialization;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SpecsController extends Controller
+{
+    public function index()
+    {
+        $jsonData = ['success' => true, 'specs' => []];
+        $specs = DB::table('specializations')->select('specializations.name')->get();
+
+
+        array_push($jsonData['specs'], $specs);
+
+
+        return response()->json($jsonData);
+    }
+}
