5b350d4a309c82d0fd7bbe65a14b6e38434a8d99
T30G10v4 <matteo.giovagnotti@gmail.com>
2023-02-13 14:58:55 +0100

    ADD: API call on users-profiles

--- app/Http/Controllers/Api/DocProfileController.php
+++ app/Http/Controllers/Api/DocProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\DocProfile;
+use App\Models\Specialization;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -11,23 +12,52 @@ class DocProfileController extends Controller
 {
     public function index()
     {
-        $users = User::all();
-        $results = [];
-        foreach ($users as $user) {
-            $docProfile = DocProfile::where('user_id', '=', $user->id)->get();
-            $userDoc = array_merge($docProfile, $user);
 
-            array_push($results, $userDoc);
+        $jsonData = ['success' => true, 'doctors' => ['specializations' => []]];
+
+        $users = User::all();
+
+        foreach ($users as $user) {
+
+            $docProfile = DocProfile::where('user_id', '=', $user->id)->get();
+
+
+            $thereIsProfile = null;
+            foreach ($docProfile as $item) {
+                $thereIsProfile = $item->id;
+            }
+
+            if ($thereIsProfile) {
+                $jsonData['doctors'][] = [
+                    'id' => $user->id,
+
+
+                    'name' => $user->name,
+                    'surname' => $user->surname,
+                    'mail' => $user->mail,
+                    'specialization' => $user->specialization,
+
+                    'profile_id' => $docProfile[0]->id,
+                    'curriculum_vitae' => $docProfile[0]->curriculum_vitae,
+                    'photo' => $docProfile[0]->photo,
+                    'studio_address' => $docProfile[0]->studio_address,
+                    'tel' => $docProfile[0]->tel,
+                    'services' => $docProfile[0]->services,
+                    'slug' => $docProfile[0]->slug,
+
+                ];
+            } else {
+                $jsonData['doctors'][] = [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'surname' => $user->surname,
+                    'mail' => $user->mail,
+                    'specialization' => $user->specialization,
+                    'profile_id' => NULL,
+                ];
+            }
         }
 
-
-
-
-        return response()->json([
-            'success' => true,
-            'results' => $results,
-
-
-        ]);
+        return response()->json($jsonData);
     }
 }
