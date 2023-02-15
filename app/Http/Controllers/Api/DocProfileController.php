41956b3257639428dd08cf131b96660d4c59e15a
Mirko Robbi <mirko.robbi@gmail.com>
2023-02-15 11:28:27 +0100

    ADJUST: review in api

--- app/Http/Controllers/Api/DocProfileController.php
+++ app/Http/Controllers/Api/DocProfileController.php
@@ -115,7 +115,7 @@ class DocProfileController extends Controller
 
             $specializationsDocArray,
 
-            $reviewsCollection,
+            "review" => $reviewsCollection,
 
         ]];
 
