f270c109dc6998cd4d6b14e7ead2f2859ee830e3
T30G10v4 <matteo.giovagnotti@gmail.com>
2023-02-17 18:06:44 +0100

    ADD: Dashboard Graphic

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,59 @@
 @extends('layouts.navigation')
 
 @section('content')
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+            integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+        <title>Document</title>
+    </head>
+
+    <body>
+        <div class="container-fluid overflow-hidden p-3">
+            <div class="row">
+                <div class="col-lg-4 col-md-12 pb-3">
+                    <div class="bg-dark rounded-4 p-3 text-light">
+                        Welcome to your Dashboard!<br>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro provident corrupti temporibus
+                        officiis blanditiis similique ratione expedita, quis dolore dignissimos, itaque consequuntur dolores
+                        debitis cumque quos in molestias odio repudiandae.
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12 pb-3">
+                    <div class="bg-primary rounded-4 p-3 text-light">Prova
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit voluptas quis molestiae eveniet. Dicta
+                        minus alias quis. Qui sunt minus, eligendi expedita harum dolorem atque placeat, dolor unde corrupti
+                        ratione.
+                        Dolorem sunt officiis, est vel maiores dolore at autem quasi perferendis voluptates eaque quae
+                        distinctio voluptatem ratione, temporibus sed minus illo sequi aliquid. Cum esse molestias totam
+                        quia reiciendis consequuntur!
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12">
+                    <div class="bg-dark rounded-4 p-3 text-light">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit aspernatur omnis aliquam
+                        distinctio deserunt aperiam, perspiciatis quaerat doloribus, corrupti magni veritatis assumenda
+                        tempora similique sint! Veritatis voluptatibus mollitia quidem error.
+                        Eius sunt et ratione? Animi eveniet deleniti magni consectetur suscipit vero odio quis maxime
+                        blanditiis, dicta dolore! Soluta voluptatem, nulla nihil nesciunt quas quia amet doloribus, mollitia
+                        consequatur recusandae tempora!
+                        Quam architecto commodi voluptate dolorem deleniti illum amet ducimus repudiandae in numquam.
+                        Accusantium enim quibusdam, facere quod animi atque sunt expedita nostrum dignissimos possimus
+                        inventore libero quia sequi suscipit sint?
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </body>
+
+    </html>
 @endsection
