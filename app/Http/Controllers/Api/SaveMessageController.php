b1deb528de0316e9c6a7cd55123df1a3c92bebea
Mirko Robbi <mirko.robbi@gmail.com>
2023-02-18 14:59:26 +0100

    ADD: try and catch message

--- app/Http/Controllers/Api/SaveMessageController.php
+++ app/Http/Controllers/Api/SaveMessageController.php
@@ -13,15 +13,21 @@ class SaveMessageController extends Controller
     public function store(StoreMessageRequest $request, $id)
     {
 
+        $doc_profile = DocProfile::find($id);
+
         $form_data = $request->validated();
 
         $message = new Message();
-        $message->doc_profile_id = $id;
+        $message->doc_profile_id = $doc_profile->id;
         $message->username = $request->input('messageUsername');
         $message->slug = DocProfile::generateSlug($message->username, '-');
         $message->mail = $request->input('messageMail');
         $message->text = $request->input('messageText');
 
-        $message->save();
+        try {
+            $message->save();
+        } catch (\Exception $e) {
+            dd($e->getMessage());
+        }
     }
 }
