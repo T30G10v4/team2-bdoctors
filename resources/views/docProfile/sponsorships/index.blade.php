f1b82867fc477f474b703c8484b56f9617b794f7
Prico3 <pricoco.andrea@gmail.com>
2023-02-21 11:02:21 +0100

    Add: index sponsor

--- resources/views/docProfile/sponsorships/index.blade.php
+++ resources/views/docProfile/sponsorships/index.blade.php
@@ -1,13 +1,20 @@
 @extends('layouts.navigation')
 
 @section('content')
-    <div class='container'>
+    <div>
+        <h1>Perchè sponsorizzare il tuo profilo? ☝</h1>
+        <h4>Con una sponsorizzazione sarai sempre in prima vista per i tuoi pazienti,
+            il tuo profilo uscirà nelle prime posizioni quando l'utente cercherà nella tua specializzazione
+        </h4>
+    </div>
+
+    <div class='container d-flex justify-content-center gap-3'>
         @foreach ($promos as $promo)
-            <div>
+            <div class="mx-4">
                 <h2>{{ $promo->name }}</h2>
                 <p>{{ $promo->price }} Euro</p>
 
-                <p>{{ $promo->duration }}</p>
+                <p>{{ $promo->duration }} h</p>
                 <button class="orderBtn">
                     <a href="{{ route('promos.show', $promo->id) }}"> Purchase </a>
                 </button>
