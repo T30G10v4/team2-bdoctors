c7aa01b9ef3bd7f453c6e24be16ba18e2128fc08
T30G10v4 <matteo.giovagnotti@gmail.com>
2023-02-21 12:34:29 +0100

    SOLVED: Date on messages

--- resources/views/docProfile/messages/show.blade.php
+++ resources/views/docProfile/messages/show.blade.php
@@ -1,15 +1,58 @@
 @extends('layouts.navigation')
 
 @section('content')
-    <div class="container">
+    {{-- <table class="table table-dark">
+        <thead>
+            <tr>
+                <p scope="col">Message from {{ $message->username }}</p>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <p>Mail: {{ $message->mail }}</p>
+            </tr>
+            <tr>
+                <p>Arrived: {{ $message->created_at }}</p>
+            </tr>
+            <tr>
+                <p>{{ $message->text }}</p>
+            </tr>
+            <tr>
+                <a class="btn btn-light" href="{{ url()->previous() }}">Back</a>
+            </tr>
+        </tbody>
+    </table> --}}
+    <table class="table table-dark">
+        <thead>
+            <tr>
+                <th scope="col">Message from {{ $message->username }}</th>
 
-        <a class="btn btn-light" href="{{ url()->previous() }}">Back</a>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">Arrived:
+                    @php
+                        //echo date_format($message->created_at, 'Y-m-d H:i:s');
+                        $date = new DateTimeImmutable($message->created_at);
+                        echo $date->format('j M o h:i');
+                        
+                    @endphp
+                </th>
 
-        <div class="row">
-            <h2>Message from {{ $message->username }}</h2>
-            <h3>Mail: {{ $message->mail }}</h3>
-            <h4>Arrived: {{ $message->created_at }}</h4>
-            <p>{{ $message->text }}</p>
-        </div>
-    </div>
+            </tr>
+            <tr>
+                <th scope="row">Mail: {{ $message->mail }}</th>
+
+            </tr>
+            <tr>
+                <th scope="row">{{ $message->text }}</th>
+
+            </tr>
+            <tr>
+                <th scope="row"><a class="btn btn-light" href="{{ url()->previous() }}">Back</a></th>
+
+            </tr>
+        </tbody>
+    </table>
 @endsection
